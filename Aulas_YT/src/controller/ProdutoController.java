cb48ca4fb4b09d0b5ad86ed0ecb24e2f55156bec
Tailor17 <tailorzitzke.pl013@academico.ifsul.edu.br>
2023-09-08 14:59:11 -0300

    Estudos

--- Aulas_YT/src/controller/ProdutoController.java
+++ Aulas_YT/src/controller/ProdutoController.java
@@ -1,22 +1,26 @@
 package controller;
 
+import model.Produto;
+
 public class ProdutoController {
     public static void main(String[] args){
-        Produto produto1 = new Produto();
-        System.out.println(produto1.nome);
-        System.out.println(produto1.valor);
-        System.out.println(produto1.estoque);
+        Produto produto1 = new Produto("Melão",10.0,10);
+        System.out.println(produto1.getNome());
+        System.out.println(produto1.getValor());
+        System.out.println(produto1.getEstoque());
         System.out.println();
 
-        Produto produto2 = new Produto();
-        System.out.println(produto2.nome);
-        System.out.println(produto2.valor);
-        System.out.println(produto2.estoque);
+        Produto produto2 = new Produto("Arroz",10.0,50);
+        System.out.println(produto2.getNome());
+        System.out.println(produto2.getValor());
+        System.out.println(produto2.getEstoque());
+        System.out.println();
+
+        Produto produto3 = new Produto("Morango",10.0,100);
+        System.out.println(produto3.getNome());
+        System.out.println(produto3.getValor());
+        System.out.println(produto3.getEstoque());
     }
 
 }
-class Produto{
-    String Arroz;
-    double valor;
-    int estoque;
-}
+
---
+++ Aulas_YT/src/model/Produto.java
@@ -0,0 +1,43 @@
+package model;
+
+public class Produto {
+    private String nome;
+    private double valor;
+    private int estoque;
+
+    public Produto() {
+    }
+
+    public Produto(String nome, double valor, int estoque) {
+        this.nome = nome;
+        this.valor = valor;
+        this.estoque = estoque;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(int estoque) {
+        int novoestoque = this.estoque + estoque;
+        if(novoestoque > 0) {
+            this.estoque = estoque;
+        }
+    }
+}
